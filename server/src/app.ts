3ebe92bed18fd35a6bc1ec30c084ef51a5c7bd70
Preston Meade <preston.meade1@gmail.com>
2021-12-29 10:07:41 -0800

    use env variables

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+require('dotenv').config();
 import "./setup/initalize"
 import { Request, Response } from 'express';
 
--- web/config/api.ts
+++ web/config/api.ts
@@ -1,1 +1,1 @@
-export const API_PATH = 'https://api.osrssocial.com/'
+export const API_PATH = process.env.IS_PROD ? 'https://api.osrssocial.com/' : 'http://localhost:6501/'
